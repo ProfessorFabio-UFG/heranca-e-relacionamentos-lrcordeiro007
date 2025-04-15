f45f7c7f241c3e88bfd4d65e20a3abde25f70e40
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-15 11:16:30 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,1 +1,32 @@
 
+public class Cliente extends PessoaFisica{
+    private double renda;
+    private String interesses;
+    private String profissao;
+
+    Cliente(String nome, String endereço, String telefone,String cpf, char sexo, int estadoCivil,double renda, String interesses, String profissao){
+        super(nome, endereço, telefone, cpf, sexo, estadoCivil);
+        this.renda = renda;
+        this.interesses = interesses;
+        this.profissao = profissao;
+    }
+    public double getRenda(){
+        return renda;
+    }
+    public void setRenda(double renda){
+        this.renda = renda;
+    }
+    public String getInteres(){
+        return interesses;
+    }
+    public void setInteres(String interesses){
+        this.interesses = interesses;
+    }
+    public String getProf(){
+        return profissao;
+    }
+    public void setProf(String profissao){
+        this.profissao = profissao;
+    }
+}
+
