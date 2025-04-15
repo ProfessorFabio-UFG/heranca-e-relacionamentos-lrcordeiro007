59791925d7d846f22ceb4d207deaa075fe80ce8c
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-15 11:17:17 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,1 +1,36 @@
 
+
+public class Funcionario extends PessoaFisica{
+    private int mat;
+    private double salario;
+    private String cargo;
+
+    Funcionario(String nome, String endereço, String telefone,
+        String cpf, char sexo, int estadoCivil,
+        int mat, double salario, String cargo){
+        super(nome, endereço, telefone, cpf, sexo, estadoCivil);
+        this.mat = mat;
+        this.salario = salario;
+        this.cargo = cargo;
+       
+    }
+
+    public int getMatricula(){
+        return mat;
+    }
+    public void setMatricula(int mat){
+        this.mat = mat;
+    }
+    public double getSalario(){
+        return salario;
+    }
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+    public String getCargo(){
+        return cargo;
+    }
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+}
