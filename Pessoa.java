296a00dca4f35cd4e645b29dc140a0d6b7d0dc3d
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-15 11:18:26 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,1 +1,31 @@
+public class Pessoa {
+    private String nome;
+    private String endereço;
+    private String telefone;
+
+    Pessoa(String nome, String endereço, String telefone){
+        this.nome = nome;
+        this.endereço = endereço;
+        this.telefone = telefone;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getEnde(){
+        return endereço;
+    }
+    public void setEnde(String endereço){
+        this.endereço = endereço;
+    }
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+}
 
