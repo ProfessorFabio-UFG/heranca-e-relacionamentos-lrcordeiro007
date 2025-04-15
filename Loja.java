4a97cb3e6783efc43563719c21f976c29fbc8bbf
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-15 11:41:43 -0300

    Update Loja.java

--- Loja.java
+++ Loja.java
@@ -1,6 +1,23 @@
-public class Loja {
+import java.util.Scanner;
 
+public class Loja {
+    Cliente clientes[] = new Cliente[2];
+    Funcionario funcionarios[] = new Funcionario[2];
+    Fornecedor fornecedores[] = new Fornecedor[2];
     public static void main(String[] args) {
+
+    }
+
+    public void cadastrarCliente(){
+        Cliente clientes[] = new Cliente(null, null, null, null, 0, 0, 0, null, null);
+    }
+    public void cadastrarFornecedor(){
+        Fornecedor fornecedores[] = new Fornecedor(String nome, String endereço, String telefone,String cnpj, String razaoSocial, String produto);
+    }
+    public void empregarFuncionario(){
+        Funcionario funcionarios[] = new Funcionario();
+    }
+    public void demitirFuncionario(){
         
     }
     
