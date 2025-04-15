9737d8b1f95a758c8c4712707176c716d69da1e3
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-15 11:16:59 -0300

    Update Fornecedor.java

--- Fornecedor.java
+++ Fornecedor.java
@@ -1,1 +1,17 @@
 
+public class Fornecedor extends Juri{
+    private String produto;
+
+    Fornecedor(String nome, String endereço, String telefone,String cnpj, String razaoSocial,String produto){
+        super(nome, endereço, telefone, cnpj, razaoSocial);
+        this.produto =produto;
+    }
+
+    public String getProduto(){
+        return produto;
+    }
+    public void setProduto(String produto){
+        this.produto = produto;
+    }
+}
+
