474807202054b777d236b0793376efb2d88b99cc
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-15 11:18:52 -0300

    Update PessoaFisica.java

--- PessoaFisica.java
+++ PessoaFisica.java
@@ -1,1 +1,35 @@
 
+public class PessoaFisica extends Pessoa{
+    private String cpf;
+    private char sexo;
+    private int estadoCivil;
+   
+
+
+    PessoaFisica(String nome, String endereço, String telefone,String cpf, char sexo, int estadoCivil){
+        super(nome, endereço, telefone);
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.estadoCivil = estadoCivil;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public char getSexo(){
+        return sexo;
+    }
+    public void setSexo(char sexo){
+        this.sexo = sexo;
+    }
+    public int getEstado(){
+        return estadoCivil;
+    }
+    public void setEstado(int estadoCivil){
+        this.estadoCivil = estadoCivil;
+    }
+}
+
