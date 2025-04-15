3b6c3a9aaffa6567debf29ce98d8b900ea9be9de
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-15 11:17:33 -0300

    Update Juri.java

--- Juri.java
+++ Juri.java
@@ -1,1 +1,24 @@
 
+public class Juri extends Pessoa{
+    private String cnpj;
+    private String razaoSocial;
+
+    Juri(String nome, String endereço, String telefone,String cnpj, String razaoSocial){
+        super(nome, endereço, telefone);
+        this.cnpj = cnpj;
+        this.razaoSocial= razaoSocial;
+    }
+    public String getCnpj(){
+        return cnpj;
+    }
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+    public String getRazao(){
+        return razaoSocial;
+    }
+    public void setRazao(String razaoSocial){
+        this.razaoSocial = razaoSocial;
+    }
+}
+
