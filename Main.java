8747e0757657e7da92b167aed04720be9e971ad7
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-04-22 10:19:38 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
 
+public class Main {
+    public static void main(String[] args) {
+        Loja loja = new Loja();
+
+        Funcionario f1 = new Funcionario("João", "Rua A", "9999-0000", "123.456.789-00", 'M', 1, 101, 2500.00, "Vendedor");
+        Cliente c1 = new Cliente("Maria", "Rua B", "9888-1111", "987.654.321-00", 'F', 2, 3000.00, "Moda", "Designer");
+        Fornecedor fr1 = new Fornecedor("Empresa X", "Av. Central", "4002-8922", "11.222.333/0001-44", "Empresa X Ltda", "Roupas");
+
+        loja.empregarFuncionario(f1);
+        loja.cadastrarCliente(c1);
+        loja.cadastrarFornecedor(fr1);
+
+        loja.listarFuncionarios();
+        loja.listarClientes();
+        loja.listarFornecedores();
+    }
+}
